6ec8fca6b502697501371bdcec4fc460b278d519
Taylor Hampton <taylor@curateurs.com>
2018-01-20 16:32:07 -0500

    javascript files

---
+++ public/javascript/nutrition.js
@@ -0,0 +1,13 @@
+// var food = "1 slice pepperoni pizza";
+// var queryURL = "https://api.nutritionix.com/v1_1/search/" + food +  
+// "?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=df6ed4b5&appKey=54fbf2d01124a030dbef2fa24d18f3d3"
+
+// $.ajax({
+//   url: queryURL,
+//   method: "GET"
+// }).done(function(response) {
+//   console.log(response);
+//   console.log(response.Runtime);
+// });
+
+console.log("we up")
