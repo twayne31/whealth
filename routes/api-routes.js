f3c36d3454622789785dd9c1505ee74c1ee1c9ec
JBW <whitco670@jbw-mbp-215.attlocal.net>
2018-01-20 11:30:35 -0500

    created CRUD api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,50 +1,80 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
 
-// Dependencies
-// =============================================================
 var models = require("../models/");
 
-
-// Routes
-// =============================================================
 module.exports = function(app) {
 
-  // Get all chirps
+//display all of ones history of logging in foods
   app.get("/api/all", function(req, res) {
-
-    // Finding all Chirps, and then returning them to the user as JSON.
-    // Sequelize queries are aynchronous, which helps with percieved speed.
-    // If we want something to be guaranteed to happen after the query, we'll use
-    // the .then function
-    models.CensusTract.findAll({}).then(function(results) {
-      // results are available to us inside the .then
+      models.foodChoice.findAll({}).then(function(results) {
       res.json(results);
-      console.log(results);
+      
     });
+   });
 
+  app.get("/api/foodChoices/createdAt/:createdAt", function(req, res) {
+    db.Post.findAll({
+      where: {
+        createdAt: req.params.createdAt
+      }
+    })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
   });
+
+  app.get("/api/foodChoices/Meal/:Meal", function(req, res) {
+    db.Post.findAll({
+      where: {
+        Meal: req.params.Meal
+      }
+    })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+
+
+  app.post("/api/foodChoices", function(req, res) {
+      console.log(req.body);
+      db.foodChoice.create({
+        Qty: req.body.QTY,
+        Unit: req.body.Unit,
+        Meal: req.body.Meal,
+        Food: req.body.Food,
+        Calories: req.body.Calories,
+        Weight: req.body.Weight,
+        Food_Group: req.body.Food_Group  
+      })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+    });   
+
+  app.put("/api/foodChoices", function(req, res) {
+    db.foodChoice.update(req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  app.delete("/api/foodChoices/:id", function(req, res) {
+    db.foodChoice.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+};
+
 }
 
-  // Add a chirp
-//   app.post("/api/new", function(req, res) {
-
-//     console.log("Chirp Data:");
-//     console.log(req.body);
-
-//     Chirp.create({
-//       author: req.body.author,
-//       body: req.body.body,
-//       created_at: req.body.created_at
-//     }).then(function(results) {
-//       // `results` here would be the newly created chirp
-//       res.end();
-//     });
-
-//   });
-
-// };
-
-
-
+ 
